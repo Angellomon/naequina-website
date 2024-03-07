11a987332335476d62750328493c26b0632413c8
Angellomon <angelmtzdiaz@gmail.com>
2024-03-07 17:19:41 -0600

    feat(lib): impl lang mappings

---
+++ src/lib/lang.js
@@ -0,0 +1,171 @@
+export const es = {
+	lang: 'es',
+	header: {
+		contactButton: 'CONTACTO'
+	},
+	jumbotron: {
+		mainTitle: {
+			first: 'SIETE AÑOS DE',
+			second: 'NAEQUINA,',
+			third: 'UNA MIRADA',
+			fourth: 'RETROSPECTIVA'
+		}
+	},
+	eventAnnouncement: {
+		leftConferenceText: {
+			conferenceNumber: '7°.',
+			firstText: 'CICLO INTERNACIONAL DE',
+			secondText: 'CONFERENCIAS'
+		},
+		rightConferenceText: {
+			firstText: 'EVENTO VIRTUAL',
+			secondText: 'GRATUITO',
+			thirdText: 'CON VALOR CURRICULAR'
+		}
+	},
+	eventDates: {
+		leftEvent: {
+			sep: 'Y',
+			month: 'ABR',
+			from: 'DE',
+			to: 'A'
+		},
+		rightEvent: {
+			title: 'INICIO DE INSCRIPCIONES',
+			date: '18 DE MARZO',
+			hour: '00:00 H',
+			from: 'DESDE LAS'
+		}
+	},
+	eventContent: {
+		title: 'CONTENIDO TEMÁTICO',
+		firstElement: ['APARATO DIGESTIVO', 'EVOLUCIÓN, MICROBIOMA Y DESÓRDENES DIGESTIVOS'],
+		secondElement: ['ALIMENTOS', 'Y NECESIDADES DE', 'NUTRIENTES'],
+		thirdElement: ['ALIMENTACIÓN', 'PARA LA VIDA'],
+		fourthElement: ['ALIMENTACIÓN', 'DEL POTRO'],
+		fifthElement: ['BIENESTAR ANIMAL,', 'LA BUENA ALIMENTACIÓN'],
+		rightText: 'MESA REDONDA CON EXPERTOS EN ALIMENTACIÓN Y SALUD EQUINA'
+	},
+	eventContactInfo: {
+		title: 'INFORMES Y COORDINADORES',
+		doctrsList: [
+			{
+				name: 'DRA. SILVIA ELENA BUNTINX DIOS',
+				email: ''
+			},
+			{
+				name: 'DR. SANTIAGO GARCÍA PASQUEL',
+				email: 'santiago.garcia@uvmnet.edu'
+			},
+			{
+				name: 'DR. MARIANO HERNÁNDEZ GIL',
+				email: ''
+			},
+			{
+				name: 'DR. EDUARDO MORONES',
+				email: 'lalovaquero@yahoo.com.mx'
+			},
+			{
+				name: 'DRA. AURORA HILDA RAMÍREZ PÉREZ',
+				email: 'rpereza@unam.mx'
+			},
+			{
+				name: 'DR. ELÍAS VELÁZQUEZ CANTÓN',
+				email: 'evelazquez@gponutec.com'
+			}
+		]
+	},
+	eventContact: {
+		title: 'CONTÁCTANOS',
+		namePlaceholder: 'Nombre',
+		emailPlaceholder: 'Correo',
+		messagePlaceholder: 'Comentarios',
+		submitText: 'ENVIAR'
+	}
+};
+
+export const en = {
+	lang: 'en',
+	header: {
+		contactButton: 'CONTACT'
+	},
+	jumbotron: {
+		mainTitle: {
+			first: 'SEVEN YEARS OF',
+			second: 'NAEQUINA,',
+			third: 'A RETROSPECTIVE',
+			fourth: 'LOOK'
+		}
+	},
+	eventAnnouncement: {
+		leftConferenceText: {
+			conferenceNumber: '7TH.',
+			firstText: 'INTERNATIONAL',
+			secondText: 'CONFERENCE SERIES'
+		},
+		rightConferenceText: {
+			firstText: 'FREE VIRTUAL',
+			secondText: 'EVENT WITH',
+			thirdText: 'CURRICULAR VALUE'
+		}
+	},
+	eventDates: {
+		leftEvent: {
+			sep: 'AND',
+			month: 'APR',
+			from: 'FROM',
+			to: 'TO'
+		},
+		rightEvent: {
+			title: 'REGISTRATION STARTS ON',
+			date: 'MARCH 18',
+			hour: '12:00 A.M.',
+			from: 'AT'
+		}
+	},
+	eventContent: {
+		title: 'THEME CONTENT',
+		firstElement: ['DIGESTIVE SYSTEM', 'EVOLUTION, MICROBIOME, AND DIGESTIVE DISORDERS'],
+		secondElement: ['FOODS', 'AND NUTRIENT', 'REQUIREMENTS'],
+		thirdElement: ['NOURISHMENT', 'FOR LIFE'],
+		fourthElement: ['DIET', 'OF THE FOAL'],
+		fifthElement: ['ANIMAL WELFARE,', 'GOOD NUTRITION'],
+		rightText: 'ROUND TABLE WITH EXPERTS IN EQUINE NUTRITION AND HEALTH'
+	},
+	eventContactInfo: {
+		title: 'INQUIRIES AND COORDINATORS',
+		doctrsList: [
+			{
+				name: 'DR. SILVIA ELENA BUNTINX DIOS',
+				email: ''
+			},
+			{
+				name: 'DR. SANTIAGO GARCÍA PASQUEL',
+				email: 'santiago.garcia@uvmnet.edu'
+			},
+			{
+				name: 'DR. MARIANO HERNÁNDEZ GIL',
+				email: ''
+			},
+			{
+				name: 'DR. EDUARDO MORONES',
+				email: 'lalovaquero@yahoo.com.mx'
+			},
+			{
+				name: 'DR. AURORA HILDA RAMÍREZ PÉREZ',
+				email: 'rpereza@unam.mx'
+			},
+			{
+				name: 'DR. ELÍAS VELÁZQUEZ CANTÓN',
+				email: 'evelazquez@gponutec.com'
+			}
+		]
+	},
+	eventContact: {
+		title: 'CONTACT US',
+		namePlaceholder: 'Name',
+		emailPlaceholder: 'Email',
+		messagePlaceholder: 'Message',
+		submitText: 'SEND'
+	}
+};
