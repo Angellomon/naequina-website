654b2408b665c9454b9a4bb362d3683043b84dab
Angellomon <angelmtzdiaz@gmail.com>
2024-03-08 13:01:40 -0600

    fix(message): fix correct text

--- src/lib/jumbotron/message.svelte
+++ src/lib/jumbotron/message.svelte
@@ -13,15 +13,24 @@
 			duration: 500,
 			delay: 800
 		}}
-		class="my-1 flex flex-col gap-2 justify-center text-white"
+		class="my-1 flex flex-col gap-2 justify-start items-start text-white"
 	>
 		<p class="bold font-bold text-xl leading-3 tracking-wide">
 			{lang.eventAnnouncement.rightConferenceText.firstText}
 		</p>
-		<p class="text-4xl leading-6">{lang.eventAnnouncement.rightConferenceText.secondText}</p>
-		<p class="bold font-bold text-sm tracking-wide">
-			{lang.eventAnnouncement.rightConferenceText.thirdText}
-		</p>
+		{#if lang.lang === 'es'}
+			<p class="text-4xl leading-6">{lang.eventAnnouncement.rightConferenceText.secondText}</p>
+
+			<p class="bold font-bold text-sm tracking-wide">
+				{lang.eventAnnouncement.rightConferenceText.thirdText}
+			</p>
+		{:else}
+			<p class="text-5xl leading-7">{lang.eventAnnouncement.rightConferenceText.secondText}</p>
+
+			<p class="bold font-bold text-xs tracking-wide">
+				{lang.eventAnnouncement.rightConferenceText.thirdText}
+			</p>
+		{/if}
 	</div>
 </OnMount>
 
