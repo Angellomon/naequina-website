399c661ec2626b9b9c2cec5bd1935eff3cd76f0e
Angellomon <angelmtzdiaz@gmail.com>
2024-03-12 00:58:46 -0600

    feat(eventbrite): impl button component

---
+++ src/lib/eventbrite-button.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { onMount } from 'svelte';
+
+	var exampleCallback = function () {
+		console.log('Pedido completo');
+	};
+
+	onMount(() => {
+		window.EBWidgets.createWidget({
+			widgetType: 'checkout',
+			eventId: '861021718137',
+			modal: true,
+			modalTriggerElementId: 'eventbrite-widget-modal-trigger-861021718137',
+			onOrderComplete: exampleCallback
+		});
+	});
+</script>
+
+<svelte:head>
+	<script src="https://www.eventbrite.com.mx/static/widgets/eb_widgets.js"></script>
+
+	<!-- Contenido Noscript para SEO adicional -->
+	<noscript
+		><a
+			href="https://www.eventbrite.com.mx/e/naequina-2024-tickets-861021718137"
+			rel="noopener noreferrer"
+			target="_blank">Comprar boletos en Eventbrite</a
+		></noscript
+	>
+</svelte:head>
+<!-- Puedes personalizar este botón como quieras -->
+<button id="eventbrite-widget-modal-trigger-861021718137" type="button">Comprar boletos</button>
