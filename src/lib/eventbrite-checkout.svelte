28221a9152ee02b539b6a542d629013afd6d123c
Angellomon <angelmtzdiaz@gmail.com>
2024-03-12 00:58:56 -0600

    feat(eventbrite): impl checkout component

---
+++ src/lib/eventbrite-checkout.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { onMount } from 'svelte';
+
+	var exampleCallback = function () {
+		console.log('Pedido completo');
+	};
+
+	onMount(() => {
+		window.EBWidgets.createWidget({
+			// Required
+			widgetType: 'checkout',
+			eventId: '861021718137',
+			iframeContainerId: 'eventbrite-widget-container-861021718137',
+
+			// Optional
+			iframeContainerHeight: 425, // Widget height in pixels. Defaults to a minimum of 425px if not provided
+			onOrderComplete: exampleCallback // Method called when an order has successfully completed
+		});
+	});
+</script>
+
+<svelte:head>
+	<script src="https://www.eventbrite.com.mx/static/widgets/eb_widgets.js"></script>
+</svelte:head>
+
+<section class="">
+	<div id="eventbrite-widget-container-861021718137"></div>
+</section>
+
+<style>
+	section {
+		min-height: 50vh;
+	}
+</style>
