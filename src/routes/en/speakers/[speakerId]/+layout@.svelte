399c769665adc6ef3de209218ff8abbe157e030b
Angellomon <angelmtzdiaz@gmail.com>
2024-03-22 19:46:58 -0600

    feat(en/speakers): impl speakers route

---
+++ src/routes/en/speakers/[speakerId]/+layout@.svelte
@@ -0,0 +1,31 @@
+<script>
+	import { onNavigate } from '$app/navigation';
+
+	onNavigate((navigation) => {
+		// @ts-ignore
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			// @ts-ignore
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
+</script>
+
+<svelte:head>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-MFR4TLDGTS"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+		gtag('config', 'G-MFR4TLDGTS');
+	</script>
+</svelte:head>
+
+<slot />
---
+++ src/routes/en/speakers/[speakerId]/+page.svelte
@@ -0,0 +1,42 @@
+<script>
+	import Footer from '$lib/footer.svelte';
+
+	export let data;
+
+	const { speaker } = data;
+</script>
+
+<div class="bg-black min-h-screen flex flex-col justify-between">
+	<div
+		class="flex flex-row align-middle justify-between items-center w-full bg-gradient-to-r from-torch-red to-black px-5"
+	>
+		<a href="/en"><img class="self-start max-w-52" src="/logo.png" alt="" /></a>
+		<h1 class=" sm:text-[1.5rem] text-white text-right">
+			{#if speaker.pre}
+				{speaker.pre}.
+			{/if}
+			{speaker.name}
+		</h1>
+	</div>
+
+	<div
+		class="flex flex-col justify-start items-center bg-black p-10 sm:px-20 lg:px-30 2xl:px-80 gap-7 min-h-full"
+	>
+		<img class="max-h-[40vh]" src={`/img/ponentes/${speaker.id}_cv.png`} alt={speaker.name} />
+
+		<div class="text-white text-justify">
+			{@html speaker.cv}
+		</div>
+	</div>
+
+	<div class="flex-0 flex flex-row justify-end relative w-100 h-56 bg-transparent -mt-20">
+		<img class="w-100 h-56" src="/img/CABALLO-NAEQUINA-1.png" alt="CABALLO NAEQUINA" />
+	</div>
+	<Footer />
+</div>
+
+<style>
+	h1 {
+		font-family: 'Montserrat Bold';
+	}
+</style>
