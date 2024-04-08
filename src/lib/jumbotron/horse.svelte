e7513fdf8b6f02f61104361d41756465529aa2df
Angellomon <angelmtzdiaz@gmail.com>
2024-04-08 11:14:43 -0600

    feat: add options

--- src/lib/jumbotron/horse.svelte
+++ src/lib/jumbotron/horse.svelte
@@ -1,6 +1,13 @@
 <script>
-	import { fade, fly, blur } from 'svelte/transition';
+	import { fade } from 'svelte/transition';
 	import { OnMount } from 'fractils';
+
+	export let bottom = false;
+
+	let horseStyle =
+		'z-5 absolute bottom-0 right-0 min-w-[120px] md:min-w-[350px] xl:min-w-[450px] 2xl:min-w-[550px] w-1/6';
+
+	if (!bottom) horseStyle = `${horseStyle} md:bottom-48 xl:bottom-32`;
 </script>
 
 <OnMount>
@@ -9,7 +16,7 @@
 			duration: 1000,
 			delay: 500
 		}}
-		class="z-5 absolute bottom-0 md:bottom-48 xl:bottom-32 right-0 min-w-[120px] md:min-w-[350px] xl:min-w-[450px] 2xl:min-w-[550px] w-1/6"
+		class={horseStyle}
 	>
 		<div class="horse relative">
 			<img class="filter brightness-75" src="/img/CABALLO-NAEQUINA-1.png" alt="caballo" />
