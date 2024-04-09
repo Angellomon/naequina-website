37f767cb962dc9ddf6a8cc50690283766742b635
Angellomon <angelmtzdiaz@gmail.com>
2024-04-09 03:38:33 -0600

    feat(net): impl api calls

--- src/routes/constancias/+page.svelte
+++ src/routes/constancias/+page.svelte
@@ -1,9 +1,14 @@
 <script>
-	import { handleCaptchaError, resetCaptcha } from '$lib/captcha';
 	import Horse from '$lib/jumbotron/horse.svelte';
+	import { downloadCertificatePDF } from '$lib/requests';
+	import Spinner from '$lib/spinner.svelte';
 
 	/** @type {string} */
 	let email;
+
+	let invalidEmail = false;
+	let emailNotFound = false;
+	let loading = false;
 
 	const recaptchaSiteKey = import.meta.env.VITE_RECAPTCHA_SITE_KEY;
 
@@ -13,29 +18,18 @@
 			// @ts-ignore
 			grecaptcha.execute(recaptchaSiteKey, { action: 'submit' }).then(function (token) {
 				// Add your logic to submit to your backend server here.
-				handleEmail(token);
+				loading = true;
+				invalidEmail = false;
+				emailNotFound = false;
+
+				downloadCertificatePDF(email, token).then((errors) => {
+					invalidEmail = errors.invalidEmail;
+					emailNotFound = errors.notFound;
+					loading = false;
+				});
 			});
 		});
 	};
-
-	/** @param token {string} */
-	async function handleEmail(token) {
-		/** @type RequestInit */
-		let options = {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify({
-				email,
-				recaptchaToken: token
-			})
-		};
-
-		const res = await fetch('/certificate-pdf', options);
-
-		console.log(res);
-	}
 </script>
 
 <div
@@ -57,10 +51,31 @@
 				placeholder="Ingresa tu correo"
 				class="bg-black text-white border border-solid border-torch-red px-4 py-2 text-xl w-full"
 			/>
+
+			{#if invalidEmail}
+				<div class="text-white">Tu correo no es válido</div>
+			{/if}
+
+			{#if emailNotFound}
+				<div class="text-white">Tu correo no se encuentra en la base de datos</div>
+			{/if}
+
+			<!-- {#if loading}
+				<div class="self-end">
+					<Spinner />
+				</div>
+			{/if} -->
+
 			<button
 				class="bg-torch-red text-white px-6 py-2 self-end cursor-pointer hover:underline"
-				on:click={handleOnSubmit}>ENVIAR</button
+				on:click={handleOnSubmit}
 			>
+				{#if loading}
+					CARGANDO
+				{:else}
+					ENVIAR
+				{/if}
+			</button>
 
 			<p class="uppercase message text-center w-4/6 py-5 lg:w-1/2">
 				INGRESA EL CORREO ELECTRÓNICO CON EL CUAL <span class="text-torch-red">TE REGISTRASTE</span>
